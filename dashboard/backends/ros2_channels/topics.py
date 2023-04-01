b3f019d0903c65c21cd765239259360d07428e8b
Daniel Vaz <danielvaz@gmail.com>
2023-04-01 11:14:58 -0300

    piadda word case corrected. improvements on topic field decomposition, listening childs of messages, until reach a primitive type.

--- dashboard/backends/ros2_channels/topics.py
+++ dashboard/backends/ros2_channels/topics.py
@@ -11,14 +11,66 @@ def list_ros2_topics():
     return topics
 
 
+class ROS2Field(object):
+    def __init__(self, field, field_type, parent=None):
+        self.name = field
+        self.field_type = field_type
+        self.children = []
+        self.parent = parent
+        self.process()
+
+    def process(self):
+        '''process field_type:
+        1. if it is a primitive type, no children will be added.
+        2. if it is a custom message, the message fields will be added.'''
+        # FIXME: needs to handle sequence<MsgType>
+        try:
+            msg = get_message(self.field_type)
+            for field, field_type in msg.get_fields_and_field_types().items():
+                self.children.append(ROS2Field(field, field_type, self))
+        except:
+            print('Error: when decoding field:', self.name,
+                  'of msg type', self.field_type)
+            pass
+
+    def get_tree_map(self):
+        child_maps = []
+        if self.is_leaf:
+            return self.name
+        for child in self.children:
+            child_maps.append(child.get_tree_map())
+        if self.is_root and len(child_maps) == 0:
+            return {self.name: self.field_type}
+        tree_map = {}
+        for m in child_maps:
+            tree_map['.'.join([self.name, m])] = m
+        return tree_map
+
+    @property
+    def is_root(self):
+        return self.parent == None
+
+    @property
+    def is_leaf(self):
+        return not len(self.children) and not self.is_root
+
+    def pretty_print(self):
+        pass
+
+
 class ROS2Topic(object):
     def __init__(self, topic):
         self.name = topic[0]
         self.msg_types = topic[1]
-        self.fields = {}
+        self.fields = []
+        self.fields_tree = {}
         for msg_type in self.msg_types:
             msg = get_message(msg_type)
-            self.fields[self.name+msg_type] = msg.get_fields_and_field_types()
+            msg_map = {}
+            for field, field_type in msg.get_fields_and_field_types().items():
+                self.fields.append(ROS2Field(field, field_type))
+                msg_map.update(self.fields[-1].get_tree_map())
+            self.fields_tree[self.name+msg_type] = msg_map
 
 
 class DataChannel(object):
